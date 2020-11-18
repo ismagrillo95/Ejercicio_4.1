fbc1fa93fe140b8102028f5840f072d74d932e4d
rr <rr@Ismael-PC.home>
2020-11-18 20:42:43 +0100

    Version 4

--- ejercicio_4_1/src/AleatorioEmple.java
+++ ejercicio_4_1/src/AleatorioEmple.java
@@ -46,6 +46,7 @@ public class AleatorioEmple {
 	}// fin verporconsola
 
 	public static void crear() throws IOException {
+	    System.out.println("Texto de prueba 3");
 		File fichero = new File("AleatorioEmple.dat");
 		RandomAccessFile file = new RandomAccessFile(fichero, "rw");
 		String apellido[] = { "FERNANDEZ", "GIL", "LOPEZ", "RAMOS", "SEVILLA", "CASILLA", "REY" };
