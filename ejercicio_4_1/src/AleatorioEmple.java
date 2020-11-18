45e3bd7e46159fe3ea31263a6dd9d3d1cdf3fbc3
rr <rr@Ismael-PC.home>
2020-11-18 20:37:42 +0100

    Version 2

--- ejercicio_4_1/src/AleatorioEmple.java
+++ ejercicio_4_1/src/AleatorioEmple.java
@@ -7,6 +7,7 @@ public class AleatorioEmple {
 	public static void main(String[] args) throws IOException {
 		crear();
 		leer();
+		System.out.println("Texto de prueba 1");
 	}
 
 	public static void leer() throws IOException {
